c20d0fd7cf5f7a3cd8bd1eb07031ecc44b3e0df5
pswang <pengshengwang@gmail.com>
2018-08-31 23:51:39 +0800

    add isMobile

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 // userAgent
-const ua = navigator.userAgent
-export const isAndroid = () => ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1
-export const isIOS = () => !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
-export const isWeixin = () => /micromessenger/i.test(ua)
+const ua = navigator.userAgent;
+export const isAndroid = () => ua.indexOf('Android') > -1 || ua.indexOf('Adr') > -1;
+export const isIOS = () => !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+export const isWeixin = () => /micromessenger/i.test(ua);
+export const isMobile = () => !!ua.match(/AppleWebKit.*Mobile.*/);
+
+export default {
+  isAndroid,
+  isIOS,
+  isMobile,
+  isWeixin
+};
